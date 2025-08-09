cb6f51f65b8b4ec4ae23c90ad28e68ee87f4f1da
ISLAH-Hamza <islah.hamza1997@gmail.com>
2025-08-09 22:24:23 +0100

    feat: impliment zebra and jigsaw sudoku puzzles to test quacq

---
+++ src/QUACQ/benchmarks/jigsaw.py
@@ -0,0 +1,92 @@
+from QUACQ.core import *
+import random
+
+
+
+class Model:
+    def __init__(self) -> None:
+        
+        self.domain=[1,6]
+        self.variables=[Variable(f'{i}',self.domain) for i in range(36)]
+        random.shuffle(self.variables)
+        self.language={
+            '==':Relation('==',2,False),
+            '!=':Relation('!=',2,False),
+            '>=':Relation('>=',2,True),
+            '<=':Relation('<=',2,True),
+            }
+        
+        self.bais=Bais(Language=self.language.values(),variables=self.variables)
+
+
+        self.constraints={
+            Constraints(scope=[self.variables[1],self.variables[6]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[1],self.variables[12]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[2],self.variables[6]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[2],self.variables[12]],relation=self.language['!=']),
+            
+            Constraints(scope=[self.variables[3],self.variables[11]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[3],self.variables[17]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[4],self.variables[11]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[4],self.variables[17]],relation=self.language['!=']),
+            
+            
+            Constraints(scope=[self.variables[7],self.variables[14]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[7],self.variables[15]],relation=self.language['!=']),
+            
+            
+            Constraints(scope=[self.variables[8],self.variables[16]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[8],self.variables[21]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[8],self.variables[22]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[9],self.variables[16]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[9],self.variables[20]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[9],self.variables[22]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[10],self.variables[20]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[10],self.variables[21]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[16],self.variables[20]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[16],self.variables[21]],relation=self.language['!=']),
+            
+            Constraints(scope=[self.variables[19],self.variables[26]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[19],self.variables[27]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[19],self.variables[28]],relation=self.language['!=']),
+            
+            Constraints(scope=[self.variables[18],self.variables[31]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[18],self.variables[32]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[24],self.variables[31]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[24],self.variables[32]],relation=self.language['!=']),
+            
+            
+            Constraints(scope=[self.variables[23],self.variables[33]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[23],self.variables[34]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[29],self.variables[33]],relation=self.language['!=']),
+            Constraints(scope=[self.variables[29],self.variables[34]],relation=self.language['!=']),
+        
+
+        }
+
+
+        #### add line clicks      
+        self.click(0,5) 
+        self.click(6,11) 
+        self.click(12,17) 
+        self.click(18,23) 
+        self.click(24,29) 
+        self.click(30,35)
+
+        #### add column clicks
+        self.click(0,30) 
+        self.click(1,31) 
+        self.click(2,32) 
+        self.click(3,33) 
+        self.click(4,34) 
+        self.click(5,35) 
+        
+      
+    
+    def click(self,start,end):
+        step=int((end-start)/5)
+        for i in range(start,end,step):
+            for j in range(i+step,end+1,step):
+                self.constraints.add(
+                    Constraints(scope=[self.variables[i],self.variables[j]],relation=self.language['!='])
+                )
---
+++ src/QUACQ/benchmarks/zebra.py
@@ -0,0 +1,59 @@
+from QUACQ.core import *
+import random
+
+
+class Model:
+    def __init__(self) -> None:
+        
+        self.domain=[0,4]
+        self.variables=[Variable(f'{i}',self.domain) for i in range(25)]
+        random.shuffle(self.variables)
+        self.indexes={
+            'Nor':self.variables[0] ,'En':self.variables[1],'Sp': self.variables[2],'Uk':self.variables[3],'Jp':self.variables[4],
+            'blue':self.variables[5] ,'red':self.variables[6],'green': self.variables[7],'yellow':self.variables[8],'Ivre':self.variables[9],
+            'cofee':self.variables[10] ,'milk':self.variables[11],'tee': self.variables[12],'jus':self.variables[13],'water':self.variables[14],
+            'fox':self.variables[15] ,'dog':self.variables[16],'snail': self.variables[17],'hors':self.variables[18],'zebra':self.variables[19],
+            'kool':self.variables[20] ,'parl':self.variables[21],'old': self.variables[22],'luck':self.variables[23],'chest':self.variables[24],
+        }
+        self.language={
+            '==':Relation('==',2,False),
+            '!=':Relation('!=',2,False),
+            '>':Relation('>=',2,True),
+            '==1':Relation('==val',1,False,1),
+            '==2':Relation('==val',1,False,2),
+            '||==1':Relation('||==val',2,False,1),
+            }
+        
+        self.bais=Bais(Language=self.language.values(),variables=self.variables)
+
+
+        self.constraints={
+            Constraints([self.indexes['Nor']],self.language['==1']),
+            Constraints([self.indexes['Nor'],self.indexes['blue']],self.language['||==1']),
+            Constraints([self.indexes['milk']],self.language['==2']),
+            Constraints([self.indexes['En'],self.indexes['red']],self.language['==']),
+            Constraints([self.indexes['cofee'],self.indexes['green']],self.language['==']),
+            Constraints([self.indexes['kool'],self.indexes['yellow']],self.language['==']),
+            Constraints([self.indexes['Ivre'],self.indexes['green']],self.language['||==1']),
+            Constraints([self.indexes['Ivre'],self.indexes['green']],self.language['>']),
+            Constraints([self.indexes['Sp'],self.indexes['dog']],self.language['==']),
+            Constraints([self.indexes['Uk'],self.indexes['tee']],self.language['==']),
+            Constraints([self.indexes['Jp'],self.indexes['parl']],self.language['==']),
+            Constraints([self.indexes['old'],self.indexes['snail']],self.language['==']),
+            Constraints([self.indexes['luck'],self.indexes['jus']],self.language['==']),
+            Constraints([self.indexes['chest'],self.indexes['fox']],self.language['||==1']),
+            Constraints([self.indexes['kool'],self.indexes['hors']],self.language['||==1']),
+        }
+
+              
+        self.click(self.variables[0:5],relation=self.language['!=']) 
+        self.click(self.variables[5:10],relation=self.language['!=']) 
+        self.click(self.variables[10:15],relation=self.language['!=']) 
+        self.click(self.variables[15:20],relation=self.language['!=']) 
+        self.click(self.variables[20:25],relation=self.language['!=']) 
+      
+        
+    def click(self,variables,relation):
+        for i in range(len(variables)):
+            for j in range(i+1,len(variables)):
+                self.constraints.add(Constraints(scope=[variables[i],variables[j]],relation=relation))
