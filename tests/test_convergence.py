d3dbde7a0b2d44f3675913144129ac8bba2d6f61
ISLAH-Hamza <islah.hamza1997@gmail.com>
2025-12-11 12:53:52 +0100

    update the tests

--- tests/test_convergence.py
+++ tests/test_convergence.py
@@ -5,29 +5,27 @@ from quacq.acquisition import QuAcq, Solve
 import warnings
 import sys
 
+# we run the convergence tests multiple times to reduce the flakiness due to the random nature of the learning algorithm and 
+# check the convergence of our implimentation
+
+@pytest.mark.parametrize("iteration", range(20))
+def test_murder_convergence(iteration):
+    murder_puzzle=murder.Model()
+    target_network=Target_Network(murder_puzzle.constraints)
+    L=QuAcq(murder_puzzle.bais,variables=murder_puzzle.variables,target_network=target_network)
+    sol=Solve(L,murder_puzzle.variables)
+    
+    assert len(sol)>0
 
 
+@pytest.mark.parametrize("iteration", range(20))
+def test_zebra_convergence(iteration):
+    zebra_puzzle=zebra.Model()
+    target_network=Target_Network(zebra_puzzle.constraints)
+    L=QuAcq(zebra_puzzle.bais,variables=zebra_puzzle.variables,target_network=target_network)
+    sol=Solve(L,zebra_puzzle.variables)
 
-def test_murder_convergence():
-    for _ in range(10):
-        murder_puzzle=murder.Model()
-        target_network=Target_Network(murder_puzzle.constraints)
-        L=QuAcq(murder_puzzle.bais,variables=murder_puzzle.variables,target_network=target_network)
-        sol=Solve(L,murder_puzzle.variables)
-        
-        assert len(sol)>0
-
-
-
-
-def test_zebra_convergence():
-    for _ in range(10):
-        zebra_puzzle=zebra.Model()
-        target_network=Target_Network(zebra_puzzle.constraints)
-        L=QuAcq(zebra_puzzle.bais,variables=zebra_puzzle.variables,target_network=target_network)
-        sol=Solve(L,zebra_puzzle.variables)
-
-        assert len(sol)>0
+    assert len(sol)>0
 
 
 if __name__ == "__main__":
